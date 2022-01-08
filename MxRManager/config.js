b53f100cb7f69d614d513e39fb487858bfccfb43
sarwin_ <sarwinpersonal2002@gmail.com>
2022-01-08 15:43:10 +0400

    Added auto-moderation (shady links and mass mentions)

--- MxRManager/config.js
+++ MxRManager/config.js
@@ -3,5 +3,25 @@ dotenv.config()
 
 module.exports = {
     prefix: '.',
-    botToken: process.env.BOT_TOKEN
+    botToken: process.env.BOT_TOKEN,
+
+    //for auto-moderation
+    massMentionThreshold: 5,
+
+
+    shadyLinks: [
+      'https://dlscord',
+      'https://discorcl',
+      'https://discord-nitro',
+      'https://discorcl.click',
+      'https://discorgift.click',
+      'https://Discrodsteam',
+
+      'http://dlscord',
+      'http://discorcl',
+      'http://discord-nitro',
+      'http://discorcl.click',
+      'http://discorgift.click',
+      'http://Discrodsteam',
+    ]
   };
